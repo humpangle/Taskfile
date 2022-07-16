cd97baed96d9b0fb36b433c02735d128c13455eb
Kanmii <maneptha@gmail.com>
2022-07-16 18:33:10 +0200

    Add helper functions/quoting and 2 space indents

--- task-completion.bash
+++ task-completion.bash
@@ -1,8 +1,8 @@
 #/bin/bash
 
 _task_completions() {
-    words="$(task help | cut -f 1 | grep -P '\w+')"
-    COMPREPLY=($(compgen -W "$words" "${COMP_WORDS[1]}"))
+  words="$(task help | cut -f 1 | grep -P '\w+')"
+  COMPREPLY=($(compgen -W "$words" "${COMP_WORDS[1]}"))
 }
 
 # Support both `run` and `task` aliases
